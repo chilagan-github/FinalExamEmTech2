0b7eb49fa72da642e6cc8752e918856d7d7b4eda
chilagan-github <carlohidekiilagan@gmail.com>
2025-05-21 11:29:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image, ImageOps
 import os
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D, Dense, Dropout, BatchNormalization
 
 st.set_page_config(
     page_title="Fashion Classifier",
